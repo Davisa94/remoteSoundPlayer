b53d90fb4037dbc72948b7677ababca55a99afaf
Davisa94 <Austin.i.davis94@gmail.com>
2021-09-02 23:48:37 -0700

    Switched to absolute paths; updated devlog

--- sub_modules/playlocalsound.py
+++ sub_modules/playlocalsound.py
@@ -1,17 +1,26 @@
 from playsound import playsound
 from pydub import AudioSegment
 from pydub.playback import play
+import os
 
 
 # given the filename return the appropriate path:
 def filenameToPath(filename):
-    return f"../{filename}"
+    cwd = os.getcwd()
+    print(cwd)
+    cwd = cwd.split("\sub_modules")
+    cwd = cwd[0]
+    cwd = cwd.replace("\\", "/")
+    print( f"{cwd}/{filename}")
+    return f"{cwd}/{filename}"
+    # return filename
 
 
 def playLocalFile(filename):
     path = filenameToPath(filename)
-    sound = AudioSegment.from_mp3(path)
+    # sound = AudioSegment.from_mp3(path)
     playsound(path)
+    # play(sound)
 
-play("bestcry.mp3")
+playLocalFile("bestcrie.mp3")
 # TODO: add a loop that tries to play the filename without extension given an extensionless filename
