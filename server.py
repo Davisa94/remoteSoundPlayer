a5f9bcdcf46bc838a70658eac9798d21d29a9148
Davisa94 <Austin.i.davis94@gmail.com>
2021-09-02 22:52:37 -0700

    trying a different audio lib since the first one cant open some files

--- server.py
+++ server.py
@@ -8,11 +8,11 @@ from sub_modules import playlocalsound as pls
 
 def main():
     print("in main")
-    beansProcess = multiprocessing.Process(target=pls.play, args=["beans.mp3"])
-    bestCryProcess = multiprocessing.Process(target=pls.play, args=["bestcry.mp3"])
-    beansProcess.start()
+    # beansProcess = multiprocessing.Process(target=pls.play, args=["beans.mp3"])
+    bestCryProcess = multiprocessing.Process(target=pls.playLocalFile, args=["bestcry.mp3"])
+    # beansProcess.start()
     bestCryProcess.start()
-    beansProcess.join()
+    # beansProcess.join()
     bestCryProcess.join()
 
 
--- sub_modules/playlocalsound.py
+++ sub_modules/playlocalsound.py
@@ -1,4 +1,6 @@
 from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 
 
 # given the filename return the appropriate path:
@@ -6,9 +8,10 @@ def filenameToPath(filename):
     return f"../{filename}"
 
 
-def play(filename):
+def playLocalFile(filename):
     path = filenameToPath(filename)
+    sound = AudioSegment.from_mp3(path)
     playsound(path)
 
-
+play("bestcry.mp3")
 # TODO: add a loop that tries to play the filename without extension given an extensionless filename
