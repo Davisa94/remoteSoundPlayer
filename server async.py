c6be452616fe65622e65edbfba83c71c90112968
Davisa94 <Austin.i.davis94@gmail.com>
2021-09-04 01:22:29 -0700

    cleaned up server file

--- server async.py
+++ server async.py
@@ -6,22 +6,11 @@ import asyncio
 # importlib.import_module("playlocalsound")
 from sub_modules import servermodulev2 as sm
 
-def mpMethod():
-    print("in main")
-    beansProcess = multiprocessing.Process(target=pls.playLocalFile, args=["beans.mp3"])
-    bestCryProcess = multiprocessing.Process(target=pls.playLocalFile, args=["bruh.mp3"])
-    beansProcess.start()
-    bestCryProcess.start()
-    beansProcess.join()
-    bestCryProcess.join()
 
 def run_server():
     sm.run()
 
 def main():
-    # with concurrent.futures.ProcessPoolExecutor as executor:
-    #     future1 = executor.submit(pls.playLocalFile, "bruh.mp3")
-    #     print(future1.result())
     run_server()
 
 if __name__ == "__main__":
